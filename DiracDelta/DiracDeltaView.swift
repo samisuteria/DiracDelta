2c7fc548e61d78c22967a5af3a412568cfb106a6
samisuteria <sami.suteria@gmail.com>
2016-01-03 22:41:11 -0800

    added a Demo App for faster debugging

--- DiracDelta/DiracDeltaView.swift
+++ DiracDelta/DiracDeltaView.swift
@@ -3,19 +3,43 @@ import ScreenSaver
 
 class DiracDeltaView: ScreenSaverView {
 
+    let currentAgeTextField = NSTextField()
+    
     // MARK: - Init
     
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
+        
+        setup()
+        setupViews()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Setup 
+    
+    private func setup() {
+        
+    }
+    
+    private func setupViews() {
+        currentAgeTextField.editable = false
+        currentAgeTextField.selectable = false
+        currentAgeTextField.textColor = .whiteColor()
+        currentAgeTextField.font = NSFont(name: "HelveticaNeue-Thin", size: 48)
+        
+        //make textfield background clear
+        currentAgeTextField.bezeled = false
+        currentAgeTextField.drawsBackground = false
+        
+        addSubview(currentAgeTextField)
+    }
+    
     override func drawRect(dirtyRect: NSRect) {
         super.drawRect(dirtyRect)
-        NSColor.greenColor().setFill()
+        NSColor.turquoiseColor().setFill()
         NSRectFill(dirtyRect)
     }
     
---
+++ DiracDeltaDemo/AppDelegate.swift
@@ -0,0 +1,33 @@
+import Cocoa
+import ScreenSaver
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
+
+    var window: NSWindow!
+    
+    // MARK: - NSApplicationDelegate
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        window.delegate = self
+        
+        guard let diracDeltaView = DiracDeltaView(frame: NSZeroRect, isPreview: false) else { return }
+        
+        window.contentView?.addSubview(diracDeltaView)
+        diracDeltaView.frame = window.contentView!.bounds
+        
+        diracDeltaView.startAnimation()
+        NSTimer.scheduledTimerWithTimeInterval(diracDeltaView.animationTimeInterval,
+            target: diracDeltaView, selector: "animateOneFrame", userInfo: nil, repeats: true)
+        
+    }
+    
+    // MARK: - NSWindowDelegate
+    
+    func windowWillClose(notification: NSNotification) {
+        // quit the app if the main window is closed
+        NSApplication.sharedApplication().terminate(window)
+    }
+
+}
+
